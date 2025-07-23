0f23d579777d486acb62f79b21baeac231e9085e
wjkim <wjkim@WONJOON>
2025-07-23 15:55:18 +0900

    로그인&로그아웃 시 페이지 이동 처리 / 회원가입 중복 처리

--- travel_community/src/main/java/com/boot/commu/mapper/UserMapper.java
+++ travel_community/src/main/java/com/boot/commu/mapper/UserMapper.java
@@ -10,8 +10,12 @@ public interface UserMapper {
 	// 로그인을 시도하는 메서드.
 	Users login(@Param("email") String email, @Param("password") String password);
 	
-	// 로그인 시도 후 해당 이메일이 db에 있는지 확인하는 메서드.
+	// DB에 이메일이 있는지 확인하는 메서드.
 	int chkId(@Param("email") String email);
+	
+	// DB에 전화번호 / 닉네임이 있는지 확인하는 메서드.
+	int chkPhone(String phone);
+	int chkNickname(String nickname);
 	
 	// users dto 객체를 매개변수로 DB users 테이블에 추가하는 메서드.
 	int addUser(Users dto);
