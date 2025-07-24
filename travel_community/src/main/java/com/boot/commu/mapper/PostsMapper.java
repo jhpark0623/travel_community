ddc397fbaf6632c88d2822de24aaf94fc97fa1a2
jujimin-jason <jason001201@gmail.com>
2025-07-24 10:23:18 +0900

    상세페이지 제작(미완성)

--- travel_community/src/main/java/com/boot/commu/mapper/PostsMapper.java
+++ travel_community/src/main/java/com/boot/commu/mapper/PostsMapper.java
@@ -1,9 +1,19 @@
 package com.boot.commu.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
+import com.boot.commu.model.PostsDetailDTO;
 
 @Mapper
 public interface PostsMapper {
+
+    // 게시글 ID로 상세 정보 조회 (JOIN 포함)
+    PostsDetailDTO getPostDetailById(int id);
+
+    // 게시글 조회수 1 증가
+    void view_countup(int id);
+    
+    // 게시글 삭제
+    void softDeletePost(int id);
+
 
 }
